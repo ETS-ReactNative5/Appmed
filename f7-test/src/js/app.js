2374d2476ccd8eae10f0f5f64c02ca4f8b426d08
Christophe Schranz <christophe.schranz@heig-vd.ch>
2020-04-02 11:37:01 +0200

    framework7 test application added

---
+++ f7-test/src/js/app.js
@@ -0,0 +1,29 @@
+import Dom7 from 'dom7';
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+// Import F7 Styles
+import 'framework7/css/framework7.bundle.css';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.css';
+
+// Import Routes
+import routes from './routes.js';
+
+// Import main app component
+import App from '../app-test.f7.html';
+
+var $$ = Dom7;
+
+var app = new Framework7({
+  root: '#app', // App root element
+  component: App, // App main component
+
+  name: 'Test app', // App name
+  theme: 'auto', // Automatic theme detection
+
+
+  // App routes
+  routes: routes,
+});
