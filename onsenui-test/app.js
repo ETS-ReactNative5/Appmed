1d4f34666c31cd936ec330d90df1051c24f84617
Christophe Schranz <christophe.schranz@heig-vd.ch>
2020-04-03 14:38:42 +0200

    Onsen UI App test first draft

---
+++ onsenui-test/app.js
@@ -0,0 +1,26 @@
+const c = console.log;
+var name = '';
+var hasname = false;
+
+const confirmName = (e) => 
+{
+    $('[data-name]').text($('[input-id="name"] input')[0].value)
+    $('[data-hasName]').addClass('visible')
+}
+
+const resetName = () => {
+    $('[input-id="name"] input')[0].value = '';
+}
+
+const deleteName = () => {
+    name = '';
+    $('[data-hasName]').removeClass('visible')
+    resetName();
+}
+
+$(document).ready(function () {
+    let target = $('[data-hasName]');
+    if (target.data('hasname')){
+      target.addClass('visible')
+    }   
+})
