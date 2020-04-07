6ecb8db35ced859bc78618787bbd8082021e07e4
Christophe Schranz <christophe.schranz@heig-vd.ch>
2020-04-07 11:03:39 +0200

    Ionic files cleaned

--- ionic-test/app-test/src/app/home/home.page.ts
+++ ionic-test/app-test/src/app/home/home.page.ts
@@ -1,30 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  name = "";
+  enteredText = "";
 
-  name = '';
-  enteredText = '';
-  
   constructor() {}
 
   confirmName() {
-    console.log(this.enteredText)
-    if(!this.enteredText) return;
+    console.log(this.enteredText);
+    if (!this.enteredText) return;
     this.name = this.enteredText;
   }
 
   resetName() {
-    this.enteredText = ''
+    this.enteredText = "";
   }
 
   deleteName() {
-    this.enteredText = ''
-    this.name = ''
+    this.enteredText = "";
+    this.name = "";
   }
-
 }
